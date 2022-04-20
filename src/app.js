7cc4693a6762825e755fc3b0a6557838b401984c
Jonatandb <jonatandb@gmail.com>
2022-04-20 02:30:27 -0300

    Ruta post a /tasks y tests
    Uso de middleware express.json() para poder leer response.body
    
    Agregado de paquete uuid para generación de id's únicos

--- src/app.js
+++ src/app.js
@@ -1,9 +1,24 @@
 import express from 'express'
+import { v4 } from 'uuid'
 
 const app = express()
 
+app.use(express.json())
+
 app.get('/tasks', (req, res) => {
-  res.send({ tasks: [] })
+  res.send([])
+})
+
+app.post('/tasks', (req, res) => {
+  const { title, description } = req.body
+
+  if (!title || !description) return res.status(400).send()
+
+  res.json({
+    title,
+    description,
+    id: v4(),
+  })
 })
 
 app.get('/ping', (req, res) => {
--- tests/index.spec.js
+++ tests/index.spec.js
@@ -7,8 +7,41 @@ describe('GET /tasks', () => {
     expect(response.statusCode).toBe(200)
   })
 
-  it('shoudl respond with an array', async () => {
+  it('should respond with an array', async () => {
     const response = await request(app).get('/tasks').send()
     expect(response.body).toBeInstanceOf(Array)
+  })
+})
+
+describe('POST /tasks', () => {
+  describe('Given a title and description', () => {
+    const newTask = {
+      title: 'Test task',
+      description: 'Test description',
+    }
+
+    it('should respond with a 200 status code', async () => {
+      const response = await request(app).post('/tasks').send(newTask)
+      expect(response.statusCode).toBe(200)
+    })
+
+    it('should have content type application/json', async () => {
+      const response = await request(app).post('/tasks').send(newTask)
+      expect(response.headers['content-type']).toEqual(
+        expect.stringContaining('json'),
+      )
+    })
+
+    it('should respond with a Task ID', async () => {
+      const response = await request(app).post('/tasks').send(newTask)
+      expect(response.body.id).toBeDefined()
+    })
+  })
+
+  describe('When title and description are missing', () => {
+    it('should respond with a 400 status code', async () => {
+      const response = await request(app).post('/tasks').send({})
+      expect(response.statusCode).toBe(400)
+    })
   })
 })
